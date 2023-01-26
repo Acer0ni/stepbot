62ab86cb0ed078929f86ac593f895b5ef27a0f3b
Marc-Antoine <mabmarchand@gmail.com>
2023-01-25 22:25:16 -0500

    Reaction to user emote - Artzx

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 
-bot = commands.Bot(command_prefix="!",intents =intents)
+bot = commands.Bot(command_prefix="/",intents =intents)
 
 @bot.event
 async def on_ready():
--- stepbot/commands/apply.py
+++ stepbot/commands/apply.py
@@ -1,10 +1,24 @@
 from discord.ext import commands
 
-
 class Apply(commands.Cog):
 
     @commands.command(name="apply")
-    async def cmd_apply(self,ctx):
-      rules_message =  await ctx.author.send("insert rules here")
-      await rules_message.add_reaction("👍")
-      await rules_message.add_reaction("👎")
+    async def cmd_apply(self,ctx, clan_name: str):
+        if clan_name == "abnormal":
+            rules_message = await ctx.author.send("insert rules here abnormal")
+            await rules_message.add_reaction("👍")
+            await rules_message.add_reaction("👎")
+        elif clan_name == "paradox":
+            rules_message = await ctx.author.send("insert rules here other_clan")
+            await rules_message.add_reaction("👍")
+            await rules_message.add_reaction("👎")
+        else:
+            await ctx.author.send("Invalid clan name.")
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction, user):
+        if user.bot:
+            return
+        if reaction.emoji == "👍":
+            await user.send("Here is the application form https://docs.google.com/forms/d/e/1FAIpQLSfurAyq_etyKwBl6guqHMCqK_U5CoxZX41mpYAanIXwACbK7g/viewform?usp=sf_link")
+        elif reaction.emoji == "👎":
+            await user.send("Thanks for your interest in our clan.")
