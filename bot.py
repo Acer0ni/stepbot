54ff1dccdb1206e72ab850df03d21e9113acdc1f
Larry Flint <79536506+LjFlint@users.noreply.github.com>
2023-02-02 13:25:10 -0800

    removed sync from bot on_ready and made it a command

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import os
 import asyncio
 from stepbot.commands.apply.command import Apply
 from stepbot.commands.ap.command import Ap
+from stepbot.commands.admincommands.command import AdminCommands
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -14,19 +15,17 @@ GUILD_ID = os.getenv("GUILD_ID")
 
 bot = commands.Bot(command_prefix="/",intents =intents)
 
-@bot.tree.command(name="test",description="a test command")
-async def first_slash_command(interaction: discord.Interaction,clan:str):
-    await interaction.response.send_message(f"{clan=}")
 
 @bot.event
 async def on_ready():
 
-    await bot.tree.sync(guild=discord.Object(id=GUILD_ID))
+    
     print(f"{bot.user.name} has connect to Discord:\n")
 
 async def setup(bot:commands.bot):
     print("setup ran")
     await bot.add_cog(Apply(bot),guild=discord.Object(id=GUILD_ID))
+    await bot.add_cog(AdminCommands(bot),guild=discord.Object(id=GUILD_ID))
     await Ap.setup(bot)
 
 asyncio.run(setup(bot))
---
+++ stepbot/commands/admincommands/command.py
@@ -0,0 +1,22 @@
+import discord
+import os
+from discord.ext import commands
+
+
+
+
+class AdminCommands(commands.Cog):
+
+
+    def __init__(self,bot:commands.bot):
+        self.bot = bot
+        self.GUILD_ID = int(os.getenv("GUILD_ID"))
+
+    
+    @commands.command(name = "sync")
+    async def cmd_sync(self,ctx:commands.context):
+        if "developer" not in [role.name.lower() for role in ctx.author.roles]:
+            await ctx.send("command not authorized")
+        else:
+            await self.bot.tree.sync(guild=discord.Object(id=self.GUILD_ID))
+            print("tree synced")
