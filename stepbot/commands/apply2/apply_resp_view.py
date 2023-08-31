a76c6db5b482dfb06b792cba7208b33d5b2c6e70
Marc-Antoine <mabmarchand@gmail.com>
2023-08-30 22:10:06 -0400

    Security logic on apply2

--- stepbot/commands/apply2/apply_resp_view.py
+++ stepbot/commands/apply2/apply_resp_view.py
@@ -5,6 +5,9 @@ import traceback
 import datetime
 
 class ApplicationResponseView(discord.ui.View):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.timeout = None
     async def get_applicant_from_interaction(self, interaction: discord.Interaction):
         guild = interaction.guild
         embed = interaction.message.embeds[0]
@@ -22,51 +25,70 @@ class ApplicationResponseView(discord.ui.View):
 
         return applicant,channel,fields_values
     
+    def has_leader_role(self, member: discord.Member) -> bool:
+        leader_role = member.guild.get_role(int(os.getenv("LEADER_ROLE_ID")))
+        return leader_role in member.roles
 
 
     @discord.ui.button(label="Approve", style=discord.ButtonStyle.green, custom_id="approve")
     async def approve_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        applicant, channel,field_values = await self.get_applicant_from_interaction(interaction)
-        embed = interaction.message.embeds[0]
-        try:
-            print(applicant)
-            button.label = "Application accepted"
-            await interaction.message.edit(view=self)
-            await applicant.send(f"Accepted. You can now apply to {embed.footer.text}. Please apply in game {applicant.mention}!")
-            await interaction.response.send_message(f"{applicant.mention} you have been accepted to {embed.footer.text}, please apply in game.")
-        except discord.Forbidden:
-            await channel.send(f"{applicant.mention} you have been accepted to {embed.footer.text}, please apply in game.")
-            print("forbidden exception")
-        except discord.HTTPException:
-            await channel.send(f"{applicant.mention} you have been accepted to {embed.footer.text}, please apply in game.")
-            print("http exception")
-            #print(f'{reaction.users} has accepted {name} to {embed.footer.text} at {date.strftime("%m/%d/%Y")}')
+        if self.has_leader_role(interaction.user):
+            applicant, channel,field_values = await self.get_applicant_from_interaction(interaction)
+            embed = interaction.message.embeds[0]
+            try:
+                button.label = "Application accepted"
+                self.timeout = 60
+                await DenialReasonModal.disable_buttons(self)
+                await interaction.message.edit(view=self)
+                await applicant.send(f"Accepted. You can now apply to {embed.footer.text}. Please apply in game {applicant.mention}!")
+                await interaction.response.send_message(f"{applicant.mention} you have been accepted to {embed.footer.text}, please apply in game.")
+            except discord.Forbidden:
+                await channel.send(f"{applicant.mention} you have been accepted to {embed.footer.text}, please apply in game.")
+                print("forbidden exception")
+            except discord.HTTPException:
+                await channel.send(f"{applicant.mention} you have been accepted to {embed.footer.text}, please apply in game.")
+                print("http exception")
+                #print(f'{reaction.users} has accepted {name} to {embed.footer.text} at {date.strftime("%m/%d/%Y")}')
+        else:
+            await interaction.response.send_message(content="You are not allowed to use this command", ephemeral=True)
+            print(f"/!\ {interaction.user.name} tried to use this command and is not allowed /!\ \n")
+            return
         
     @discord.ui.button(label="Deny", style=discord.ButtonStyle.danger, custom_id="deny")
     async def deny_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        applicant, channel,field_values = await self.get_applicant_from_interaction(interaction)
-        modal = DenialReasonModal(applicant=applicant, channel=channel)
-        await interaction.response.send_modal(modal)
-        button.label = "Application denied"
-        await interaction.message.edit(view=self)
+        if self.has_leader_role(interaction.user):
+            applicant, channel,field_values = await self.get_applicant_from_interaction(interaction)
+            modal = DenialReasonModal(applicant=applicant, channel=channel)
+            await interaction.response.send_modal(modal)
+            button.label = "Application denied"
+            await interaction.message.edit(view=self)
+            self.timeout = 60
+        else:
+            await interaction.response.send_message(content="You are not allowed to use this command", ephemeral=True)
+            print(f"/!\ {interaction.user.name} tried to use this command and is not allowed /!\ \n")
+            return
 
 
     @discord.ui.button(label="Pending", style=discord.ButtonStyle.secondary, custom_id="pending")
     async def pending_button(self, interaction: discord.Interaction, button: discord.ui.Button,):
-        applicant, channel, fields_values = await self.get_applicant_from_interaction(interaction)
-        embed = interaction.message.embeds[0]
-        try:
-            button.label = "Application waitlisted"
-            button.disabled = True
-            await interaction.message.edit(view=self)
-            await applicant.send(f'Hey {applicant.mention}, we are full right now in {embed.footer.text}, you have been added to the waiting list, and we will get back to you when there is room.')
-            await interaction.response.send_message(f'Hey {applicant.mention}, we are full right now in {embed.footer.text}, you have been added to the waiting list, and we will get back to you when there is room.')
-        except discord.Forbidden:
-            await channel.send(f'Hey {applicant.mention}, we are full right now in {embed.footer.text}, you have been added to the waiting list, and we will get back to you when there is room.')
-            print("forbidden exception")
-        except discord.HTTPException:
-            await channel.send(f'Hey {applicant.mention}, we are full right now in {embed.footer.text}, you have been added to the waiting list, and we will get back to you when there is room.')
-            print("http exception")
-            print(f"Inserting data into {embed.footer.text} waiting list")
-        button.label = "Application waitlisted"
-        insert_to_sheet(fields_values, 'stepbot', embed.footer.text)
+        if self.has_leader_role(interaction.user):
+            applicant, channel, fields_values = await self.get_applicant_from_interaction(interaction)
+            embed = interaction.message.embeds[0]
+            try:
+                button.label = "Application waitlisted"
+                button.disabled = True
+                await interaction.message.edit(view=self)
+                await applicant.send(f'Hey {applicant.mention}, we are full right now in {embed.footer.text}, you have been added to the waiting list, and we will get back to you when there is room.')
+                await interaction.response.send_message(f'Hey {applicant.mention}, we are full right now in {embed.footer.text}, you have been added to the waiting list, and we will get back to you when there is room.')
+            except discord.Forbidden:
+                await channel.send(f'Hey {applicant.mention}, we are full right now in {embed.footer.text}, you have been added to the waiting list, and we will get back to you when there is room.')
+                print("forbidden exception")
+            except discord.HTTPException:
+                await channel.send(f'Hey {applicant.mention}, we are full right now in {embed.footer.text}, you have been added to the waiting list, and we will get back to you when there is room.')
+                print("http exception")
+                print(f"Inserting data into {embed.footer.text} waiting list")
+            insert_to_sheet(fields_values, 'stepbot', embed.footer.text)
+        else:
+            await interaction.response.send_message(content="You are not allowed to use this command", ephemeral=True)
+            print(f"/!\ {interaction.user.name} tried to use this command and is not allowed /!\ \n")
+            return
--- stepbot/commands/apply2/view.py
+++ stepbot/commands/apply2/view.py
@@ -6,18 +6,15 @@ from stepbot.commands.apply2.apply_modal import *
 
 class Dropdown(discord.ui.Select):
     def __init__(self):
-        # Define the options you want in the select menu
         options = [
-                discord.SelectOption(label='Abnormal', emoji='<a:bluecheck:1145758589584162876>'),
-                discord.SelectOption(label='Paradox', emoji='<a:redcheck:1145758812414939257>'),
-                discord.SelectOption(label='Anomaly', emoji='<a:orangecheck:1145758814675669002>'),
-                discord.SelectOption(label='Paranormal', emoji='<a:greencheck:1145758816978350230>'),
+                discord.SelectOption(label='Abnormal', emoji='<a:bluecheck:1145758589584162876>', description='CP : 24m+ | Full team Prefered'),
+                discord.SelectOption(label='Paradox', emoji='<a:redcheck:1145758812414939257>', description='CP : 18m+ | Full Team Prefered'),
+                discord.SelectOption(label='Anomaly', emoji='<a:orangecheck:1145758814675669002>', description='CP : 15m+'),
+                discord.SelectOption(label='Paranormal', emoji='<a:greencheck:1145758816978350230>', description='CP : 15m+'),
             ]
-
         super().__init__(placeholder='Select an option...', min_values=1, max_values=1, options=options)
 
     async def callback(self, interaction: discord.Interaction):
-        # This is what happens when an option is selected
         clan_name = self.values[0]
         await interaction.response.send_modal(ApplyModal(clan_name=clan_name))
 
@@ -40,7 +37,6 @@ class ApplyView(discord.ui.View):
 class ClanView(commands.Cog):
     @app_commands.command(name="apply2")
     async def cmd_apply(self, interaction: discord.Interaction):
-        # Get the member who initiated the interaction
         member = interaction.user
         
         leader_role = int(os.getenv("LEADER_ROLE_ID"))
@@ -48,12 +44,10 @@ class ClanView(commands.Cog):
         leader_role = member.guild.get_role(leader_role)
         
         if leader_role in member.roles:
-            # Member has the leader role, allow them to use the command
             embed = Embed(title="Apply to one of AbNorMaL Syndicate clans", description="Welcome to the server, if you wish to apply to one of ours clans, please click on the button to start the process. ", color=0x0068cf)
             apply_view = ApplyView(embed=embed)
             await interaction.response.send_message(view=apply_view, embed=embed)
         else:
-            # Member doesn't have the leader role, send a message indicating permission denied
             await interaction.response.send_message("You don't have permission to use this command.", ephemeral=True)
 
 
